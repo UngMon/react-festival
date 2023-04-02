394e7fa0dcf9107753521dcc380a7b4391b4bdb8
UngMon <ungmorning9@gmail.com>
2023-04-02 17:04:21 +0900

    리뷰 댓글 기능 구현 완료

--- src/components/Content/Review/Reviews.tsx
+++ src/components/Content/Review/Reviews.tsx
@@ -1,5 +1,11 @@
-import { useState, useRef, useEffect } from "react";
-import { DocumentData, DocumentReference, setDoc, updateDoc, deleteField, arrayRemove } from "firebase/firestore";
+import { useState, useRef, useEffect, useCallback } from "react";
+import {
+  DocumentData,
+  DocumentReference,
+  setDoc,
+  arrayRemove,
+  updateDoc,
+} from "firebase/firestore";
 import { nowDate } from "../../../modules/NowData";
 import { firebaseState, Comment } from "../../../modules/Type";
 import Loading from "../../UI/Loading";
@@ -19,18 +25,62 @@ const Reviews = ({
 }: ReviewProps) => {
   console.log("render");
   const [reviewArray, setReviewArray] = useState<Comment[]>([]);
-  const [isVisibleOption, setIsVisibleOption] = useState<boolean>(false);
+  const [isOpenOption, setOpenOption] = useState<boolean>(false);
+  const [clickedElement, setClickedElement] = useState<HTMLElement | null>(
+    null
+  );
   const [pickedComment, setPickedComment] = useState<
-    [string, string, string | number]
-  >(["", "", ""]);
+    [string, string, string, string | number]
+  >(["", "", "", ""]);
   const inputRef = useRef<HTMLInputElement>(null);
+  const saveInputRef = useRef<HTMLInputElement>(null);
+  const optionRef = useRef<HTMLDivElement[]>([]);
   useEffect(() => {
     if (firebaseState.succesGetData) {
       setReviewArray(firebaseState.contentData[contentId].comment);
     }
   }, [firebaseState, contentId]);
 
-  const reviewInputHandler = (event: React.FormEvent) => {
+  const closeOptionModal = useCallback((event: any) => {
+    let isIn = false;
+
+    for (const divItem of optionRef.current) {
+      if (!divItem) {
+        continue;
+      }
+
+      if (divItem.contains(event.target)) {
+        isIn = true;
+        break;
+      } else {
+        isIn = false;
+      }
+    }
+
+    if (!isIn) {
+      setClickedElement(null);
+      setOpenOption(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isOpenOption) {
+      // Option창이 켜져있을 경우, 사용자가 스크롤을 못하게 방지.
+      document.body.style.cssText = "overflow: hidden";
+    } else {
+      document.body.style.cssText = "";
+    }
+
+    if (clickedElement) {
+      window.addEventListener("click", closeOptionModal);
+    }
+
+    return () => {
+      window.removeEventListener("click", closeOptionModal);
+    };
+  }, [isOpenOption, closeOptionModal, clickedElement]);
+
+  const reivewSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     if (!firebaseState.loginedUser)
       return alert("로그인 하시면 이용하실 수 있습니다.");
@@ -53,37 +103,77 @@ const Reviews = ({
 
     setDoc(contentRef, { comment: array }, { merge: true });
     setReviewArray(array);
+    inputRef.current!.value = "";
   };
 
   const optionClickHandler = (
-    itemUid: string,
-    itemWhen: string,
+    event: any,
+    userName: string,
+    userUid: string,
+    userWhen: string
   ) => {
+    if (userUid !== uid) {
+      return;
+    }
+
+    if (clickedElement === event.target) {
+      // 사용자가 전에 클릭했던 Element('review-option')가 같은 태그를 클릭했을 시,
+      setOpenOption(false);
+      setClickedElement(null);
+      setPickedComment(["", "", "", ""]);
+    } else {
+      // 다른 태그('review-option')를 클릭했을 시,
+      setOpenOption(true);
+      setClickedElement(event.target);
+      setPickedComment([userName, userUid, userWhen, ""]);
+    }
+  };
+
+  const reviseButtonHandler = (itemUid: string,index: number) => {
+    if (!firebaseState.loginedUser) {
+      return alert('로그인 하시면 이용하실 수 있습니다.');
+    }
+
     if (itemUid !== uid) {
       return;
     }
-    setIsVisibleOption((prevState) => !prevState);
-    setPickedComment([itemUid, itemWhen, ""]);
-  };
 
-  const reviseReviewHandler = (index: number) => {
-    setIsVisibleOption(false);
-    setPickedComment([pickedComment[0], pickedComment[1], index]);
+    setOpenOption(false);
+    setPickedComment([
+      pickedComment[0],
+      pickedComment[1],
+      pickedComment[2],
+      index,
+    ]);
   };
 
   const deleteReviewHandler = (index: number) => {
-    let updateData: { [key: string]: any} = {};
-    updateData[`comment[${index}]`] = arrayRemove();
-    updateDoc(contentRef, updateData);
+    updateDoc(contentRef, { comment: arrayRemove(reviewArray[index]) });
+    setReviewArray([
+      ...reviewArray.slice(0, index),
+      ...reviewArray.slice(index + 1),
+    ]);
   };
 
-  const cancelRevise = () => {
-    setPickedComment(["", "", ""]);
+  const reviseReviewHandler = () => {
+    const { year, month, date, time } = nowDate();
+    const newArray = [...reviewArray];
+    const index = pickedComment[3] as number;
+    const changedData = {
+      name: pickedComment[0],
+      text: saveInputRef.current!.value,
+      uid: pickedComment[1],
+      when: year + "-" + month + "-" + date + " " + time,
+    };
+    newArray[index] = changedData;
+    updateDoc(contentRef, { comment: newArray });
+    setReviewArray(newArray);
+    setPickedComment(['', '', '', ''])
   };
-  console.log(pickedComment);
+
   return (
     <>
-      <form className="user-input-box" onSubmit={reviewInputHandler}>
+      <form className="user-input-box" onSubmit={reivewSubmitHandler}>
         <input
           type="text"
           id="user-input"
@@ -102,25 +192,33 @@ const Reviews = ({
                 <p>{item.name[0].toUpperCase()}</p>
               </div>
 
-              {index !== pickedComment[2] && (
+              {index !== pickedComment[3] && (
                 <>
                   <div className="user-reivew-top">
                     <div className="user-name">{item.name}</div>
                     <div
                       className="review-option"
-                      onClick={() =>
-                        optionClickHandler(item.uid, item.when)
+                      onClick={(event) =>
+                        optionClickHandler(
+                          event,
+                          item.name,
+                          item.uid,
+                          item.when
+                        )
+                      }
+                      ref={(el: HTMLDivElement) =>
+                        (optionRef.current[`${index}`] = el)
                       }
                     >
                       옵션
                     </div>
-                    {isVisibleOption &&
-                      item.uid === pickedComment[0] &&
-                      item.when === pickedComment[1] && (
+                    {isOpenOption &&
+                      item.uid === pickedComment[1] &&
+                      item.when === pickedComment[2] && (
                         <div className="option-box">
                           <p
                             className="option-revise"
-                            onClick={() => reviseReviewHandler(index)}
+                            onClick={() => reviseButtonHandler(item.uid, index)}
                           >
                             수정
                           </p>
@@ -136,14 +234,16 @@ const Reviews = ({
                   <div className="review-text">{item.text}</div>
                 </>
               )}
-              {index === pickedComment[2] && (
+              {index === pickedComment[3] && (
                 <>
                   <div className="revise-input">
-                    <input placeholder={item.text}></input>
+                    <input placeholder={item.text} ref={saveInputRef}></input>
                   </div>
                   <div className="review-button-box">
-                    <button onClick={cancelRevise}>취소</button>
-                    <button>저장</button>
+                    <button onClick={() => setPickedComment(["", "", "", ""])}>
+                      취소
+                    </button>
+                    <button onClick={reviseReviewHandler}>저장</button>
                   </div>
                 </>
               )}
