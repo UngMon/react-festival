44579bc1a842b54595ff52bf680c546f912f43cf
UngMon <ungmorning9@gmail.com>
2023-04-26 00:12:03 +0900

    Firebase sdk 수정 #48

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXS3GyktvwjcLU3AeLdpEUqrNp-Tx4lKA",
-  authDomain: "festival-5a61a.firebaseapp.com",
-  databaseURL: "https://festival-5a61a-default-rtdb.firebaseio.com",
-  projectId: "festival-5a61a",
-  storageBucket: "festival-5a61a.appspot.com",
-  messagingSenderId: "677436762893",
-  appId: "1:677436762893:web:0dd9d968bd2194eb16de44"
+  apiKey: process.env.REACT_APP_FRIEBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FRIEBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FRIEBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FRIEBASE_STOREAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FRIEBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FRIEBASE_APP_ID,
 };
 
 // Initialize Firebase
