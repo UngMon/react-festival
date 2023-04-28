0cd7d3e2254f5bac5f4697c83430f4c3f1de220f
UngMon <ungmorning9@gmail.com>
2023-04-28 22:51:50 +0900

    V1.01 메인페이지 수정 후 추가및 footer css 수정 #61

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer-box">
       <p>Contact: ungmorning9@gmail.com</p>
-      <p>정보제공: 한국관광공사</p>
+      <p>제공: 한국관광공사, pexels</p>
     </footer>
   );
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import LoginButton from "./LoginButton";
 const Header = () => {
   return (
     <header className="Header-box">
-      <Link to="/month/all" className="Logo">
+      <Link to="/" className="Logo">
         <img src="/images/fetivalLogo.png" alt="logo"></img>
         <span className="logo-title">축제모아</span>
       </Link>
