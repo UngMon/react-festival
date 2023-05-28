4b5c27bcb5b7029b8c0218ea6dddad1199cb2d78
UngMon <ungmorning9@gmail.com>
2023-05-28 19:02:33 +0900

    Top.tsx 로직 완성// throttle기능 추가#102

--- src/components/header/Top.tsx
+++ src/components/header/Top.tsx
@@ -5,32 +5,39 @@ import "./Top.css";
 
 const Top = () => {
   const [isVisible, setVisible] = useState<boolean>(false);
-  const [movingToTop, setMovingToTop] = useState<boolean>(false);
+  const [throttle, setThrottle] = useState<boolean>(false);
 
   const movingTopView = () => {
-    if (!movingToTop) {
-      window.scrollTo(0, 0);
-
-      setVisible(false);
-      setMovingToTop(true);
-
-      setTimeout(() => {
-        setMovingToTop(false);
-      }, 1000);
-    }
+    window.scrollTo(0, 0);
+    setVisible(false);
   };
 
   useEffect(() => {
-    const scroll = () => {
-      if (window.scrollY < 1500 && isVisible) setVisible(false);
-      if (window.scrollY > 1500 && !isVisible) setVisible(true);
-      console.log(window.scrollY);
-    };
+    // 스로틀링을 통해 과도한 렌더링 방지
+    if (throttle) return;
 
-    if (!movingToTop) window.addEventListener("scroll", scroll);
+    // setTime에서 1초 간격으로 스로틀링의 상태를 변경한다.
+    // 만약 사용자가 1500 px높이에서 스로틀링 때문에 top버튼이 사라지지 않을 경우, 
+    // 버튼을 사라지게 만들어줌.
+    if (isVisible && window.scrollY < 1500) setVisible(false);
+
+    const scroll = () => {
+      if (window.scrollY < 1500 && isVisible) {
+        setVisible(false);
+      }
+      if (window.scrollY >= 1500 && !isVisible) {
+        setVisible(true);
+      }
+      setThrottle(true);
+      setTimeout(() => {
+        setThrottle(false);
+      }, 1000);
+    };
+    
+     window.addEventListener("scroll", scroll);
 
     return () => window.removeEventListener("scroll", scroll);
-  });
+  }, [isVisible, throttle]);
 
   return (
     <>
